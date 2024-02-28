e863b0a612feb3d16cf6f7989aa09695dc57b42e
mew <mewmewdevart@gmail.com>
2024-02-28 12:10:18 -0300

    scripts(content): add the ts files, learning about primitive types

--- Courses/Scrimba/Typescript/helloWorld.ts
+++ Courses/Scrimba/Typescript/helloWorld.ts
@@ -1,1 +1,19 @@
 console.log('Hello World!'); // Looks like  JS >:v
+
+/* What is Typescript?
+    - Superset of JS : Writing types is optional
+    - Adds static typing
+    - Compiles to JS
+    - Developed by Microsoft
+    - Open Source
+    - Great tooling, community, documentation
+    - Great support for React, Angular, Vue, etc.
+    - Browsers cant run TS, so it needs to be compiled to JS
+    - Transcompiles to JS
+
+    Pros:
+    Code that is easier to read and understand
+    Cleaner
+    More readable
+    No need to write comments to explain what the code does
+*/
