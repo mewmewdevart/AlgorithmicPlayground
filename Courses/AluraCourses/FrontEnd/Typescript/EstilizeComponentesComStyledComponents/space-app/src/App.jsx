918f00f161583e99c1ecd2cb2042f2f4b9bf5b52
Larissa <larissa.benedito@clarke.com.br>
2024-03-30 13:40:07 -0300

    scripts(feat): add the filter bar search action

--- Courses/AluraCourses/FrontEnd/Typescript/EstilizeComponentesComStyledComponents/space-app/src/App.jsx
+++ Courses/AluraCourses/FrontEnd/Typescript/EstilizeComponentesComStyledComponents/space-app/src/App.jsx
@@ -1,12 +1,10 @@
 import { useState } from "react";
-
 import { styled } from "styled-components";
 import EstilosGlobais from "./componentes/EstilosGlobais";
 import Cabecalho from "./componentes/Cabecalho";
 import BarraLateral from "./componentes/BarraLateral";
 import Banner from "./componentes/BannerEstilizado";
 import Galeria from "./componentes/Galeria";
-
 import fotos from "./fotos.json";
 import ModalZoom from "./componentes/ModalZoom";
 
@@ -23,62 +21,92 @@ const FundoGradiente = styled.div`
   justify-content: center;
 `;
 const AppContainer = styled.div`
-  width: 98%; 
+  width: 98%;
   max-width: 1440px;
 `;
 
 const MainContainer = styled.main`
   display: flex;
-  flex-direction: column; /* Display as column by default */
+  flex-direction: column;
   gap: 20px;
 
   @media screen and (min-width: 768px) {
-    flex-direction: row; /* Change to row layout for screens wider than 768px */
+    flex-direction: row;
   }
 `;
 
 const ConteudoGaleria = styled.section`
-  flex-grow: 1; /* Grow to fill available space */
+  flex-grow: 1;
 `;
 
 const App = () => {
   const [fotosDaGaleria, setFotosDaGaleria] = useState(fotos);
-  const [fotoSelecionada, setFotoSelecionada] = useState(null)
+  const [fotoSelecionada, setFotoSelecionada] = useState(null);
+  const [textoPesquisa, setTextoPesquisa] = useState("");
 
   const aoAlternarFavorito = (foto) => {
     if (foto.id === fotoSelecionada?.id) {
       setFotoSelecionada({
         ...fotoSelecionada,
-        favorita: !fotoSelecionada.favorita
+        favorita: !fotoSelecionada.favorita,
       });
     }
-  
-    setFotosDaGaleria(fotosDaGaleria.map(fotosDaGaleria => {
-      return {
-        ...fotosDaGaleria,
-        favorita: fotosDaGaleria.id === foto.id ? !foto.favorita : fotosDaGaleria.favorita
-      }
-    }))
+
+    setFotosDaGaleria(
+      fotosDaGaleria.map((fotosDaGaleria) => {
+        return {
+          ...fotosDaGaleria,
+          favorita:
+            fotosDaGaleria.id === foto.id
+              ? !foto.favorita
+              : fotosDaGaleria.favorita,
+        };
+      })
+    );
+  };
+
+  const filtrarFotos = () => {
+    return fotos.filter((foto) =>
+      Object.values(foto).some(
+        (value) =>
+          typeof value === "string" &&
+          value.toLowerCase().includes(textoPesquisa.toLowerCase())
+      )
+    );
+  };
+
+  const aoFotoSelecionada = (foto) => {
+    setFotoSelecionada(foto);
+    setFotosDaGaleria(filtrarFotos());
+  };
+
+  const aoPesquisar = (texto) => {
+    setTextoPesquisa(texto);
+    if (texto === "") {
+      setFotosDaGaleria(fotos);
+    } else {
+      setFotosDaGaleria(filtrarFotos());
+    }
   };
 
   return (
     <FundoGradiente>
       <EstilosGlobais />
       <AppContainer>
-        <Cabecalho />
+        <Cabecalho aoPesquisar={aoPesquisar} />
         <MainContainer>
           <BarraLateral />
           <ConteudoGaleria>
             <Banner />
-            <Galeria 
-              aoFotoSelecionada={foto => setFotoSelecionada(foto)}
+            <Galeria
+              aoFotoSelecionada={aoFotoSelecionada}
               aoAlternarFavorito={aoAlternarFavorito}
               fotos={fotosDaGaleria}
             />
           </ConteudoGaleria>
         </MainContainer>
       </AppContainer>
-      <ModalZoom 
+      <ModalZoom
         foto={fotoSelecionada}
         aoFechar={() => setFotoSelecionada(null)}
         aoAlternarFavorito={aoAlternarFavorito}
--- Courses/AluraCourses/FrontEnd/Typescript/EstilizeComponentesComStyledComponents/space-app/src/componentes/Cabecalho/index.jsx
+++ Courses/AluraCourses/FrontEnd/Typescript/EstilizeComponentesComStyledComponents/space-app/src/componentes/Cabecalho/index.jsx
@@ -1,27 +1,30 @@
-import {styled} from "styled-components"
-import CampoTexto from "../CampoTexto"
+import { styled } from "styled-components";
+import CampoTexto from "../CampoTexto";
 
 const HeaderEstilizado = styled.header`
-    padding: 60px 0;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    width: 100%;
-    height: 40px;
-    img {
-        width: 200px;
-        height: 65px;
-    }
-`
+  padding: 60px 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 40px;
+  img {
+    width: 200px;
+    height: 65px;
+  }
+`;
 
-const Cabecalho = () => 
-{
-    return(
-        <HeaderEstilizado>
-            <img src="/imagens/logo.png" alt="" />
-            <CampoTexto></CampoTexto>
-        </HeaderEstilizado>
-    )
-}
+const Cabecalho = ({ aoPesquisar }) => {
+  const handlePesquisa = (texto) => {
+    aoPesquisar(texto);
+  };
+
+  return (
+    <HeaderEstilizado>
+      <img src="/imagens/logo.png" alt="Logo" />
+      <CampoTexto aoAlterar={handlePesquisa} />
+    </HeaderEstilizado>
+  );
+};
 
 export default Cabecalho;
